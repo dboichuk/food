25426d6521b2eab7e5d595c1ef5f8c496f8f99f8
Dmytro Boichuk <dboichuk@mail.greenriver.edu>
2020-04-27 17:16:05 -0400

    added breakfast

--- index.php
+++ index.php
@@ -14,4 +14,13 @@ $f3->route('GET /', function (){
     echo $view->render("views/home.html");
 });
 
+$f3->route('GET /breakfast', function (){
+    $view = new Template();
+    echo $view->render("views/bfast.html");
+
+
+});
+
+
+
 $f3->run();
